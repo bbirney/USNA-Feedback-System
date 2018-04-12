339234f881735ae7232f277bffd5351711044913
bbirney <ben.birney@gmail.com>
2018-04-11 23:03:36 -0400

    added FAQ page, updated navbar

--- home/contact_us.php
+++ home/contact_us.php
@@ -10,7 +10,7 @@
                       'position'   => 99,
                       'student'    => true,
                       'instructor' => true,
-                      'guest'      => false,
+                      'guest'      => true,
                       'access'     => array());
   ###############################################################
 
@@ -26,4 +26,42 @@
   # Note: You too will have automated NavBar generation
   #       support in your future templates...
   require_once(WEB_PATH.'navbar.php');
+
+  if(isset($_REQUEST['name'])){
+    $mailHeaders = "From: " . $_REQUEST['email'] . "\r\n";
+    mail("m200516@usna.edu",$_REQUEST['name'],$_REQUEST['feedback'],$mailHeaders);
+    unset($_REQUEST);
+  }
 ?>
+
+  <div class="row text-center">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+      <br><br><br>
+      <div class="jumbotron text-center">
+        <h2>We want to hear from you!</h2>
+        <h4>Fill out the form below with questions, comments, concerns and<br> we will get back to you ASAP.</h4><br>
+        <form id="myform">
+          <div class="form-group">
+            <input type="text" name="name" class="form-control" style="max-width:50%;margin-left:auto;margin-right:auto;" placeholder="Name">
+            <br><input type="email" name="email" class="form-control" style="max-width:50%;margin-left:auto;margin-right:auto;" placeholder="Email">
+            <br><textarea class="form-control" name="feedback" style="max-width:50%;margin-left:auto;margin-right:auto;" rows="4"placeholder="Questions, Comments, Concerns"></textarea>
+            <br><button type="submit" class="btn btn-default">Submit Feedback</button>
+          </div>
+        </form>
+    </div>
+    </div>
+  </div>
+  <script type="text/javascript">
+    $("#myform").submit(function(e){
+    $.ajax({
+      data: $("#myform").serialize(),
+      success: function(result) {
+        $("#output").html(result);
+      }
+    });
+    e.preventDefault();
+    });
+  </script>
+</body>
+</html>
---
+++ home/faq.php
@@ -0,0 +1,86 @@
+<?php
+
+  ###############################################################
+  #              Security and Navbar Configuration              #
+  ###############################################################
+  $MODULE_DEF = array('name'       => 'Give Feedback',
+                      'version'    => 1.0,
+                      'display'    => '',
+                      'tab'        => 'give_feedback',
+                      'position'   => 99,
+                      'student'    => true,
+                      'instructor' => true,
+                      'guest'      => true,
+                      'access'     => array());
+  ###############################################################
+
+  $PAGE_TITLE = "Feedback for Feedback";
+
+  # Load in Configuration Parameters
+  require_once("../etc/config.inc.php");
+
+  # Load in template, if not already loaded
+  require_once(LIBRARY_PATH.'template.php');
+
+  # Load in The NavBar
+  # Note: You too will have automated NavBar generation
+  #       support in your future templates...
+  require_once(WEB_PATH.'navbar.php');
+?>
+  <h1 class="text-center">What is Good Feedback?</h1>
+  <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+      <br>
+      <div class="jumbotron">
+        <ol>
+          <li>
+            Be Direct
+            <ul>
+              <li>
+                People are often afraid of being direct with negative feedback and they try to cushion the negative feedback by not openly addressing problems. Be honest, direct, and respectful.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Be Specific
+            <ul>
+              <li>
+                Always offer concrete examples or facts to back up feedback. Generalizations are good for evaluations; however the purpose of feedback is for the individual to improve. Providing specifics allows the receiver to better focus on the problem and remedy it.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Positive feedback is equally important as  negative feedback
+            <ul>
+              <li>
+                The purpose of positive feedback is to allow a midshipman to identify what he/she is doing right and to capitalize on his/her strengths.
+              </li>
+              <li>
+                Positive feedback is not just "Good Job." Positive feedback is specifically identifying what the person is doing right. Ex: " You are a really good squad leader" is not feedback. "Good work in having weekly fire team meetings; those meetings promote unity among the squad and are good opportunities to train and pass word" is feedback.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Goal referenced
+            <ul>
+              <li>
+                Feedback should always be in reference to an overall goal. For midshipmen, this goal is becoming commissioned officers. When identifying a problem, make sure to point out why it is counteractive to the goal the team is aiming for.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Respect
+            <ul>
+              <li>
+                Keep things objective; injecting emotion or anger into feedback distracts from its meaning and often makes things less direct or vague. Attacking people for their faults will not help them improve; objectively pointing out what they need to work on will help them improve.
+              </li>
+            </ul>
+          </li>
+        </ol>
+    </div>
+    </div>
+  </div>
+
+</body>
+</html>
