fc91fdbaafe7f8d3eb835456ba597c1adfa33cd4
bbirney <m200516@usna.edu>
2018-04-20 09:47:04 -0400

    Wrote msg board API script

--- api/scripts/board.php
+++ api/scripts/board.php
@@ -1,7 +1,21 @@
 <?php
   if (!isset($API_PATH)) die();
+  if (!isset($_POST['msg']) || !isset(USER['user'])) die();
 
-  $msg = array($API_PATH[0]);
+  $query_fields = array(USER['user'], $_POST['msg']);
+  $stmt = build_query($db, "INSERT INTO message_board (user, msg) VALUES (?, ?)", $query_fields);
+  $stmt->close();
 
-  
+  $stmt = build_query($db, "SELECT * FROM message_board", array());
+  $stmt->bind_result($user, $time, $msg);
+
+  $mb_content = array();
+  for ($i=0;$stmt->fetch();$i++) {
+    $mb_content[$i]['user'] = $user;
+    $mb_content[$i]['time'] = $time;
+    $mb_content[$i]['msg'] = $msg;
+  }
+
+  echo json_encode($mb_content);
+  die();
 ?>
