45ef28bff140cc644c3ebd7e5fd95ced49dd7270
bbirney <ben.birney@gmail.com>
2018-04-23 16:54:40 -0400

    Updated form (with required fields)

--- home/give_feedback.php
+++ home/give_feedback.php
@@ -27,11 +27,11 @@
   #       support in your future templates...
   require_once(WEB_PATH.'navbar.php');
 
-  if (isset($_REQUEST['feedback']) && isset($_REQUEST['alpha'])) {
+  if (isset($_REQUEST['do_well']) && isset($_REQUEST['alpha'])&& isset($_REQUEST['improve'])) {
     if (!isset($_REQUEST['good_bad'])) $_REQUEST['good_bad'] = 2;
-    $query_fields = array($_REQUEST['alpha'], $_REQUEST['feedback'], USER['user'], $_REQUEST['good_bad']);
+    $query_fields = array($_REQUEST['alpha'], $_REQUEST['do_well'], $_REQUEST['improve'], USER['user'], $_REQUEST['good_bad'], $_REQUEST['know']);
 
-    $stmt = build_query($db, "INSERT INTO feedback (user, feedback, giver, status) VALUES (?, ?, ?, ?)", $query_fields);
+    $stmt = build_query($db, "INSERT INTO feedback (user, do_well, improve, giver, status, know) VALUES (?, ?, ?, ?, ?, ?)", $query_fields);
 
     $response = "<div class=\"col-md-4 col-md-offset-4 alert alert-success text-center\" role=\"alert\">
                   <strong>Feedback submitted to ".$_REQUEST['alpha']."!</strong>
@@ -46,10 +46,10 @@
     <form id="myform">
       <input type="text" class="form-control small" name="alpha" placeholder="mAlpha" autofocus required></input><br>
       How well do you know this person?<br>
-      <label class="feedback_option">1 <input type="radio" name="know " value="1"></label>
-      <label class="feedback_option"><input type="radio" name="know" value="2"></label>
-      <label class="feedback_option"><input type="radio" name="know" value="3"></label>
-      <label class="feedback_option"><input type="radio" name="know" value="4"></label>
+      <label class="feedback_option">1 <input type="radio" name="know " value="1" ></label>
+      <label class="feedback_option"><input type="radio" name="know" value="2" ></label>
+      <label class="feedback_option"><input type="radio" name="know" value="3" ></label>
+      <label class="feedback_option"><input type="radio" name="know" value="4" ></label>
       <label class="feedback_option"><input type="radio" name="know" value="5"> 5</label><br><br>
       <textarea class="form-control" name="do_well" rows="5" placeholder="What does he/she do well?" required></textarea><br>
       <textarea class="form-control" name="improve" rows="5" placeholder="What could he/she improve?" required></textarea><br>
