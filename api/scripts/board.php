e9fc1ab0e3385df2ca0124b830a7875ad1c23356
bbirney <ben.birney@gmail.com>
2018-04-24 14:19:19 -0400

    Uses oop, ajax w/ api call

--- api/scripts/board.php
+++ api/scripts/board.php
@@ -1,21 +1,50 @@
 <?php
   if (!isset($API_PATH)) die();
-  if (!isset($_POST['msg']) || !isset(USER['user'])) die();
 
-  $query_fields = array(USER['user'], $_POST['msg']);
-  $stmt = build_query($db, "INSERT INTO message_board (user, msg) VALUES (?, ?)", $query_fields);
-  $stmt->close();
+  if (isset($_REQUEST['msg'])) {
+    $query_fields = array(USER['user'], $_REQUEST['msg']);
+    $stmt = build_query($db, "INSERT INTO message_board (user, msg) VALUES (?, ?)", $query_fields);
+    $stmt->close();
+  }
+
+  class Message {
+    public $name;
+    public $msg;
+    public $time;
+
+    function __construct($name, $msg, $time) {
+      $this->name = $name;
+      $this->msg = $msg;
+      $this->time = $time;
+    }
+
+    function create_blurb() {
+      $blurb = "<div>";
+      $blurb .=  "<div class=\"jumbotron mb ";
+      if ($this->name == USER['user']) $blurb .= "bg-primary";
+      $blurb .= "\">";
+      $blurb .= ($this->msg);
+      $blurb .= "</div><br>";
+      $blurb .= ($this->name)."<br>";
+      $blurb .= "</div>";
+
+      return $blurb;
+    }
+  }
+
 
   $stmt = build_query($db, "SELECT * FROM message_board", array());
   $stmt->bind_result($user, $time, $msg);
 
   $mb_content = array();
   for ($i=0;$stmt->fetch();$i++) {
-    $mb_content[$i]['user'] = $user;
-    $mb_content[$i]['time'] = $time;
-    $mb_content[$i]['msg'] = $msg;
+    $mb_content[$i] = new Message($user, $msg, $time);
   }
 
-  echo json_encode($mb_content);
+  for ($i=0;$i<sizeof($mb_content); $i++) {
+    echo $mb_content[$i]->create_blurb();
+  }
+
+
   die();
 ?>
